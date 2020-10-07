55f31b782d96f616473ed41fc0d0c562da712a7f
stonerains <72440538+stonerains@users.noreply.github.com>
2020-10-07 20:21:03 +0900

    Update pathplanner.py
    sr 고정

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -92,7 +92,9 @@ class PathPlanner():
     x = max(sm['liveParameters'].stiffnessFactor, 0.1)
     sr = max(sm['liveParameters'].steerRatio, 0.1)
     VM.update_params(x, sr)
-
+    
+    VM.sR = CP.steerRatio
+    
     curvature_factor = VM.curvature_factor(v_ego)
 
     self.LP.parse_model(sm['model'])
